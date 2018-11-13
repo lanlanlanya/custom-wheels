3a4f9b8e4f91a16fa94a81073841b9450900a11f
lanlanlanya <282134012@qq.com>
2018-11-13 17:52:17 +0800

    完成g-button-group样式

---
+++ button-group.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: "wheels-button-group",
+        mounted(){
+            for(let node of this.$el.children){
+                let name=node.nodeName.toLowerCase();
+                if(name!=='button'){
+                    console.warn(`g-button-group的子元素只能是g-button,但你的子元素为${name}`);
+                }
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+.g-button-group{
+    display: inline-flex;
+    vertical-align: middle;
+    >.g-button{
+        border-radius: 0;
+        &:not(:first-child){
+            margin-left:-1px;
+        }
+        &:first-child{
+            border-top-left-radius: var(--border-radius);
+            border-bottom-left-radius: var(--border-radius);
+        }
+        &:last-child{
+            border-top-right-radius: var(--border-radius);
+            border-bottom-right-radius: var(--border-radius);
+        }
+        &:hover{z-index: 1;}
+    }
+}
+</style>
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -11497,6 +11497,98 @@ render._withStripped = true
       
       }
     })();
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js","vue-hot-reload-api":"node_modules/vue-hot-reload-api/dist/index.js","vue":"node_modules/vue/dist/vue.common.js"}],"button-group.vue":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+//
+//
+//
+//
+//
+var _default = {
+  name: "wheels-button-group",
+  mounted: function mounted() {
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = this.$el.children[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var node = _step.value;
+        var name = node.nodeName.toLowerCase();
+
+        if (name !== 'button') {
+          console.warn("g-button-group\u7684\u5B50\u5143\u7D20\u53EA\u80FD\u662Fg-button,\u4F46\u4F60\u7684\u5B50\u5143\u7D20\u4E3A".concat(name));
+        }
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return != null) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+  }
+};
+exports.default = _default;
+        var $03e808 = exports.default || module.exports;
+      
+      if (typeof $03e808 === 'function') {
+        $03e808 = $03e808.options;
+      }
+    
+        /* template */
+        Object.assign($03e808, (function () {
+          var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", { staticClass: "g-button-group" }, [_vm._t("default")], 2)
+}
+var staticRenderFns = []
+render._withStripped = true
+
+          return {
+            render: render,
+            staticRenderFns: staticRenderFns,
+            _compiled: true,
+            _scopeId: "data-v-03e808",
+            functional: undefined
+          };
+        })());
+      
+    /* hot reload */
+    (function () {
+      if (module.hot) {
+        var api = require('vue-hot-reload-api');
+        api.install(require('vue'));
+        if (api.compatible) {
+          module.hot.accept();
+          if (!module.hot.data) {
+            api.createRecord('$03e808', $03e808);
+          } else {
+            api.reload('$03e808', $03e808);
+          }
+        }
+
+        
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
+      }
+    })();
 },{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js","vue-hot-reload-api":"node_modules/vue-hot-reload-api/dist/index.js","vue":"node_modules/vue/dist/vue.common.js"}],"app.js":[function(require,module,exports) {
 "use strict";
 
@@ -11506,11 +11598,15 @@ var _button = _interopRequireDefault(require("./button.vue"));
 
 var _icon = _interopRequireDefault(require("./icon.vue"));
 
+var _buttonGroup = _interopRequireDefault(require("./button-group.vue"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 _vue.default.component('g-button', _button.default);
 
 _vue.default.component('g-icon', _icon.default);
+
+_vue.default.component('g-button-group', _buttonGroup.default);
 
 new _vue.default({
   el: '#app',
@@ -11522,7 +11618,7 @@ new _vue.default({
     };
   }
 });
-},{"vue":"node_modules/vue/dist/vue.common.js","./button.vue":"button.vue","./icon.vue":"icon.vue"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"vue":"node_modules/vue/dist/vue.common.js","./button.vue":"button.vue","./icon.vue":"icon.vue","./button-group.vue":"button-group.vue"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -11549,7 +11645,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53633" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57422" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
